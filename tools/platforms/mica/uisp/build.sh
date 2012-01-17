a75ede0daa893fcb2b544af5b0a7d3ed3793c55a
Razvan Musaloiu-E <razvan@musaloiu.com>
2012-01-16 23:17:02 -0800

    Add rpm building support for uisp

--- tools/platforms/mica/uisp/build.sh
+++ tools/platforms/mica/uisp/build.sh
@@ -3,28 +3,33 @@
 UISP_VER=20050519tinyos
 UISP=uisp-${UISP_VER}
 
-ARCH_TYPE=$(dpkg-architecture -qDEB_HOST_ARCH)
 if [[ "$1" == deb ]]
 then
+    ARCH_TYPE=$(dpkg-architecture -qDEB_HOST_ARCH)
     PREFIX=$(pwd)/${UISP}/debian/usr
-    PACKAGES_DIR=$(pwd)/../../../../packages/${ARCH_TYPE}
+    PACKAGES_DIR=$(pwd)/../../../../packages/debian/${ARCH_TYPE}
     mkdir -p ${PACKAGES_DIR}
 fi
-: ${PREFIX:=$(pwd)/../../../../local}
 
+if [[ "$1" == rpm ]]
+then
+    PREFIX=$(pwd)/${UISP}/fedora/usr
+fi
+
+: ${PREFIX:=$(pwd)/../../../../local}
 
 build()
 {
     set -e
     (
 	./bootstrap
-    	./configure --prefix=${PREFIX}
-    	make
-    	make install-strip
+	./configure --prefix=${PREFIX}
+	make
+	make install-strip
     )
 }
 
-package()
+package_deb()
 {
     echo Packaging ${UISP}
     cd ${UISP}
@@ -33,9 +38,22 @@ package()
     mkdir -p debian/DEBIAN
     cat ../uisp.control \
 	| sed 's/@version@/'${UISP_VER}-`date +%Y%m%d`'/' \
-        | sed 's/@architecture@/'${ARCH_TYPE}'/' \
-        > debian/DEBIAN/control
+	| sed 's/@architecture@/'${ARCH_TYPE}'/' \
+	> debian/DEBIAN/control
     dpkg-deb --build debian ${PACKAGES_DIR}/uisp-${UISP_VER}.deb
+}
+
+package_rpm()
+{
+    echo Packaging ${UISP}
+    find ${UISP}/fedora/usr/bin/ -type f \
+	| xargs perl -i -pe 's#'${PREFIX}'#/usr#'
+    rpmbuild \
+	-D "version ${UISP_VER}" \
+	-D "release `date +%Y%m%d`" \
+	-D "prefix ${PREFIX}" \
+	-bb uisp-tinyos.spec
+
 }
 
 case $1 in
@@ -49,7 +67,12 @@ case $1 in
 
     deb)
 	build
-	package
+	package_deb
+	;;
+
+    rpm)
+	build
+	package_rpm
 	;;
 
     *)
